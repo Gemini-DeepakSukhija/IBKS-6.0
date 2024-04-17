8440ef03868af6a59babdb2e2417e4a8ae8ffd56
Lalit.Kumar <Lalit.Kumar@GEMGM-210234>
2024-04-17 17:48:46 +0530

    Added model validation

--- IBKSTicketTrackingSystemDTO/DTOs/TicketDetail.cs
+++ IBKSTicketTrackingSystemDTO/DTOs/TicketDetail.cs
@@ -27,11 +27,15 @@ namespace IBKSTicketTrackingSystemDTO.DTOs
         /// <summary>
         /// Module id
         /// </summary>
+        [Required(ErrorMessage = "Please enter ApplicationId")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid ApplicationId")] 
         public int ApplicationId { get; set; }
 
         /// <summary>
         /// Module Name
         /// </summary>
+        [Required(ErrorMessage = "Please enter ApplicationName")]
+        [StringLength(250)] 
         public string ApplicationName { get; set; }
 
         /// <summary>
@@ -52,31 +56,43 @@ namespace IBKSTicketTrackingSystemDTO.DTOs
         /// <summary>
         /// Id of priority
         /// </summary>
+        [Required(ErrorMessage = "Please enter PriorityId")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid PriorityId")]
         public int PriorityId { get; set; }
 
         /// <summary>
         /// Name of priority
         /// </summary>
+        [Required(ErrorMessage = "Please enter Priority")]
+        [StringLength(250)] 
         public string Priority { get; set; }
 
         /// <summary>
         /// Ticket type id
         /// </summary>
+        [Required(ErrorMessage = "Please enter TicketTypeId")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid TicketTypeId")] 
         public int TicketTypeId { get; set; }
 
         /// <summary>
         /// Ticket type name
         /// </summary>
+        [Required(ErrorMessage = "Please enter TicketType")]
+        [StringLength(250)] 
         public string TicketType { get; set; }
 
         /// <summary>
         /// Status id of ticket
         /// </summary>
+        [Required(ErrorMessage = "Please enter StatusId")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid StatusId")] 
         public int StatusId { get; set; }
 
         /// <summary>
         /// Name of status
         /// </summary>
+        [Required(ErrorMessage = "Please enter Status")]
+        [StringLength(250)] 
         public string Status { get; set; }
 
         /// <summary>
